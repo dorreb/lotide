0deedfb716f5640e3c50c7e357d7357a23ba17c9
dorreb <ashley_ure@hotmail.com>
2023-01-10 16:05:44 -0500

    writing the heart of the code

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅" + ` Assertion Passed: ${actual}  === ${expected}.`);
+  } else if (actual !== expected) {
+    console.log("🛑🛑🛑" + ` Assertion Failed: ${actual} !== ${expected}.`);
+  }
+};
+
+const eqArrays = function(first, second) {
+  if (first.length !== second.length){
+    return false;
+  }
+  for (let i = 0; i < first.length; i++){
+    if (first[i] !== second[i]){
+      return false;
+    } 
+  }
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
