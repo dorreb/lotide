ab5fb0be20217166026913f533c36b4fcae66f77
dorreb <ashley_ure@hotmail.com>
2023-01-11 15:32:32 -0500

    creating function declaration

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    console.log("Assertation failed");
+  } else {
+    console.log("Assertation passed");
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (letter === " ") {
+      continue;
+    }
+    if (results[letter]) {
+      results[letter].push(i);
+    } else {
+      results[letter] = [i];
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello"), [1]);
