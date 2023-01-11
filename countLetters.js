5c35c55c3e7d54e2baa71b216fb11762fefcaba2
dorreb <ashley_ure@hotmail.com>
2023-01-11 15:06:09 -0500

    counting letters in a string

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅" + ` Assertion Passed: ${actual}  === ${expected}.`);
+  } else if (actual !== expected) {
+    console.log("🛑🛑🛑" + ` Assertion Failed: ${actual} !== ${expected}.`);
+  }
+};
+
+const countLetters = function(string) {
+  const lowerCaseString = string.toLowerCase();
+
+  let results = {};
+  for (let letter of lowerCaseString) {
+    if (letter === " ") {
+      continue;
+    }
+    if (results[letter]) {
+      results[letter]++;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+console.log(countLetters("Hello world"));
+
