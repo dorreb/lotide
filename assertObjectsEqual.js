8fe5dbc1886bf7a07e23b3d2b87bc47e6d25f252
dorreb <ashley_ure@hotmail.com>
2023-01-11 19:20:08 -0500

    creating function definition

---
+++ assertObjectsEqual.js
@@ -0,0 +1,46 @@
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected) === false) {
+    console.log("🛑🛑🛑" + ` Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}.`);
+  } else {
+    console.log("✅✅✅" + ` Assertion Passed: ${inspect(actual)} === ${inspect(expected)}.`);
+  }
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red"};
+assertObjectsEqual(shirtObject , anotherShirtObject); // => true
+
