eac275ce0c6e72edddb896a65731986705e2d9c3
dorreb <ashley_ure@hotmail.com>
2023-01-16 08:09:54 -0500

    implementing modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅" + ` Assertion Passed: ${actual}  === ${expected}.`);
-  } else if (actual !== expected) {
-    console.log("🛑🛑🛑" + ` Assertion Failed: ${actual} !== ${expected}.`);
-  }
-};
-
 const eqArrays = function(first, second) {
   if (first.length !== second.length) {
     return false;
@@ -18,4 +10,4 @@ const eqArrays = function(first, second) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
